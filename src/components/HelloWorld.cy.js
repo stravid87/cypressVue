e5af9bac9788f08d3a86747e111ef1888d3dbcb9
stravid87 <ravi.student.dev@gmail.com>
2024-04-12 16:46:59 -0400

    WIP: commiting

--- src/components/HelloWorld.cy.js
+++ src/components/HelloWorld.cy.js
@@ -1,10 +1,26 @@
 import HelloWorld from './HelloWorld.vue'
 
 describe('<HelloWorld />', () => {
-  it('renders', () => {
+  beforeEach(() => {
     // see: https://on.cypress.io/mounting-vue
     cy.mount(HelloWorld)
+  })
+
+  it('renders', () => {
+    cy.get('h3')
+    cy.get('#layer8-persistence-check-btn')
+    cy.get('#check-tunnel-btn')
+  })
+
+  it('opens the tunnel', () => {
+    // see: https://on.cypress.io/mounting-vue
+    // cy.mount(HelloWorld)
     cy.get('#layer8-persistence-check-btn').click()
+  })
+
+  it('sends a post request through the tunnel', () => {
+    // see: https://on.cypress.io/mounting-vue
+    // cy.mount(HelloWorld)
     cy.get('#check-tunnel-btn').click()
   })
 })
